6b07185fb0e118b219b20c3b7c2d3f7e5fbe23b4
ouhenio <eiherrera@uc.cl>
2023-10-28 16:14:48 -0300

    update predict boilerplate

--- predict_boilerplate.py
+++ predict_boilerplate.py
@@ -1,15 +1,10 @@
 from typing import Any
-from cog import BasePredictor, Input, Path
+from cog import BasePredictor, Input
 
 class Predictor(BasePredictor):
     def setup(self):
-        """Load the model into memory to make running multiple predictions efficient"""
         self.model = None
 
-    # Define the arguments and types the model takes as input
-    def predict(self, image: Path = Input(description="Image to classify")) -> Any:
-        """Run a single prediction on the model"""
-        # Run the prediction
-        preds = self.model.predict(image)
-        # Return the top 3 predictions
-        return preds
+    def predict(self, x: str = Input(description="Model input")) -> Any:
+        self.model.predict(x)
+        return self.model.predict(x)
