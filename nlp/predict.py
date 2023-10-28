1cbd454136c0c2d58c54ca6e74c206cc78f025fd
ouhenio <eiherrera@uc.cl>
2023-10-28 16:26:30 -0300

    improve gpt2 inputs

--- nlp/predict.py
+++ nlp/predict.py
@@ -1,11 +1,19 @@
 from typing import Any
-from cog import BasePredictor
+from cog import BasePredictor, Input
 from transformers import pipeline
 
 class Predictor(BasePredictor):
     def setup(self):
+        # This is inefficient, weights are downloaded each time
         self.model = pipeline('text-generation', model='gpt2')
 
-    def predict(self, prompt: str) -> Any:
-        output = self.model(prompt, max_length=60)
+    def predict(
+            self,
+            prompt: str = Input(description="GPT2 prompt"),
+            max_length: int = Input(
+                description="Max tokens to generate",
+                default=60
+            )
+        ) -> Any:
+        output = self.model(prompt, max_length=max_length)
         return output
